305518b07a9a213ce5dc296bb57e2c7f08d4963d
Semih Cavdar <semihcavdar@Semihs-MacBook-Pro.local>
2026-01-15 23:19:48 +0300

    Add Cloudflare next-on-pages adapter

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"images.unsplash.com",
-            },
-        ],
-    },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
